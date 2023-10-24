74031a4123815f977780c1e10bea634e7c1d8149
Azizillo Nematov <aziznematov.job@gmail.com>
2023-10-24 16:29:11 +0300

    fixed Etagi scraper

---
+++ commerce_estate/Etagi/process.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from datetime import datetime
+
+path = """O:\\Nematov\\Web_scraping\\ProDevelopment\\commerce_estate\\Etagi"""
+
+path_sales = '{}\\data\\Sales.xlsx'.format(path)
+path_supply = '{}\\data\\Supply.xlsx'.format(path)
+date = f'{datetime.now().day}/{datetime.now().month}/{datetime.now().year}'
+
+class processing:
+    def write_data(self, data, path):
+        data.reset_index(drop=True).to_excel(f'{path}', index = False)
+    
+    def update_data(self, new_data):
+        try:
+            sales = pd.read_excel(path_sales)
+            supply = pd.read_excel(path_supply)
+        except:
+            self.write_data(pd.DataFrame(columns=list(new_data.columns) + ['sale_date']), path_sales)
+            self.write_data(pd.DataFrame(columns=new_data.columns), path_supply)
+            sales = pd.read_excel(path_sales)
+            supply = pd.read_excel(path_supply)
+        
+        new_supply = new_data[~new_data['url'].isin(supply['url'])]
+        sold_supply = supply[~supply['url'].isin(new_data['url'])]
+        remaining_supply = supply[supply['url'].isin(new_data['url'])]
+            
+        if not new_supply.empty:
+            save_supply = pd.concat([remaining_supply, new_supply])
+            self.write_data(save_supply, path_supply)
+        
+        if not sold_supply.empty:
+            sold_supply.loc[:, 'sale_date'] = date
+            
+            save_sales = pd.concat([sales, sold_supply])
+            self.write_data(save_sales, path_sales)
+            
+            save_supply = pd.concat([remaining_supply, new_supply])
+            self.write_data(save_supply, path_supply)
--- commerce_estate/Etagi/scraper.py
+++ commerce_estate/Etagi/scraper.py
@@ -3,6 +3,7 @@ import pandas as pd
 from datetime import datetime
 
 from headers import headers_etagi
+from process import processing
 
 def process_location(address, house_num):
         tags = {'city' : 'город',
@@ -18,49 +19,67 @@ def process_location(address, house_num):
         
         return result
 
+
 class Scraper:
     
     def __init__(self) -> None:
         self.date = f'{datetime.now().day}_{datetime.now().month}_{datetime.now().year}'
         
     def start_request(self):
-         data = []
-         
-         page = 1
-         request = headers_etagi.get_json(1)
-         
-         while request['data'] != []:
-             request = headers_etagi.get_json(page)
-             local = [
-                 {
-                    'id' : obj['object_id'],
-                    "url": f"https://spb.etagi.com/realty/{obj['object_id']}",
-                    'name' : obj['class'],
-                    'type' : obj['type'],
-                    'address' : process_location(obj['meta'], obj['house_num']),
-                    'city' : 'Санкт Петербург',
-                    'latitude' : obj['la'],
-                    'longitude' : obj['lo'],
-                    'price' : obj['price'],
-                    'price_m2' : obj['price_m2'],
-                    'square' : obj['square'],
-                    'scrape_date' : self.date,
-                    'parking' : obj['parking'],
-                    'metro_station' : obj['metro_station'],
-                    'time_to_metro' : obj['time_to_metro'],
-                    'old_price' : obj['old_price'],
-                    'floor' : obj['floor'],
-                    'floors' : obj['floors'],
-                    'location' : obj['location'],
-                    'main_photo_ur' : 'https://cdn.esoft.digital/19201080{}'.format(obj['main_photo'])
-                }
-                 for obj in data["data"]
-                ]
-             data += local
-             page += 1
+        data = []
+        
+        #Меняешь параметр, чтобы собирать данные для аренды и продажи
+        for action in ['sale', 'lease']:
+            page = 1
+            request = next(headers_etagi.get_json(1, action))
+
+            while request['data'] != []:
+                request = next(headers_etagi.get_json(page, action))
+                local = [
+                     {
+                        'id' : obj['object_id'],
+                        "url": f"https://spb.etagi.com/realty/{obj['object_id']}",
+                        'name' : obj['class'],
+                        'type' : obj['type'],
+                        'action_sl' : obj['action_sl'],
+                        'address' : process_location(obj['meta'], obj['house_num']),
+                         'district' : obj['meta']['district'],
+                        'city' : obj['meta']['city'],
+                        'latitude' : obj['la'],
+                        'longitude' : obj['lo'],
+                        'price' : obj['price'],
+                        'price_m2' : obj['price_m2'],
+                        'square' : obj['square'],
+                        'scrape_date' : self.date,
+                        'parking' : obj['parking'],
+                        'metro_station' : obj['metro_station'],
+                        'time_to_metro' : obj['time_to_metro'],
+                        'old_price' : obj['old_price'],
+                        'floor' : obj['floor'],
+                        'floors' : obj['floors'],
+                        'location' : obj['location'],
+                        'main_photo_ur' : 'https://cdn.esoft.digital/19201080{}'.format(obj['main_photo'])
+                    }
+                     for obj in request["data"]
+                    ]
+                data += local
+                print(f'Собрана информация с {page} страницы по {action}')
+                page += 1
              
-         yield data
+        yield data
              
-             
-new_data = Scraper().start_request()
-print(next(new_data))
+def main():
+    print('Go')
+    scraper = Scraper()
+    processor = processing()
+    
+    data = next(scraper.start_request())
+    new_data = pd.DataFrame(data)
+    print(f'Всего собрано {new_data.shape[0]} объявлений')
+    print(f'Включены объявления  из городов: {", ".join(new_data["city"].unique())}')
+    processor.update_data(new_data)
+    
+    print('End')
+    
+if __name__ == "__main__":
+   main()
